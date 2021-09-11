a53fcf4dc48ce25f25c1329565bdff2bf979ea42
Fernando o. Pimenta <fernando.pimenta107@gmail.com>
2021-09-10 21:54:32 -0300

    Update mfa-delete.sh

--- s3-advanced/mfa-delete.sh
+++ s3-advanced/mfa-delete.sh
@@ -2,9 +2,9 @@
 aws configure --profile root-mfa-delete-demo
 
 # enable mfa delete
-aws s3api put-bucket-versioning --bucket mfa-demo-stephane --versioning-configuration Status=Enabled,MFADelete=Enabled --mfa "arn-of-mfa-device mfa-code" --profile root-mfa-delete-demo
+aws s3api put-bucket-versioning --bucket nomedoseubucket --versioning-configuration Status=Enabled,MFADelete=Enabled --mfa "arn-of-mfa-device mfa-code" --profile root-mfa-delete-demo
 
 # disable mfa delete
-aws s3api put-bucket-versioning --bucket mfa-demo-stephane --versioning-configuration Status=Enabled,MFADelete=Disabled --mfa "arn-of-mfa-device mfa-code" --profile root-mfa-delete-demo
+aws s3api put-bucket-versioning --bucket nomedoseubucket --versioning-configuration Status=Enabled,MFADelete=Disabled --mfa "arn-of-mfa-device mfa-code" --profile root-mfa-delete-demo
 
 # delete the root credentials in the IAM console!!!
